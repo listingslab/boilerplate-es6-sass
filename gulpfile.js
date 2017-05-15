344c3772761d9f80c1b2010ccbacb5c6b5245d1b
Chris Dorward <listingslab@gmail.com>
2017-05-16 09:19:15 +0930

    Improve gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@
     gulpfile.js
  */
 
-const gulp          = require('gulp');
-const babelify      = require('babelify');
-const browserSync   = require('browser-sync').create();
-const connect       = require('gulp-connect');
-const browserify    = require('browserify');
-const source        = require('vinyl-source-stream');
+const gulp = require('gulp');
+const babelify = require('babelify');
+const browserify = require('browserify');
+const browserSync = require('browser-sync').create();
+const connect = require('gulp-connect');
+const notify = require("gulp-notify");
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
 
 gulp.task('default',['serve']);
 
@@ -16,22 +20,30 @@ gulp.task('serve', ['js'], function() {
     browserSync.init({
         server: './public'
     });
-    gulp.watch('src/*.js', ['js']);
     gulp.watch('public/*.html').on('change', browserSync.reload);
     gulp.watch('public/css/*.css').on('change', browserSync.reload);
+    gulp.watch('src/*.js', ['js']);
 });
-
 
 // Transpile ES6 JavaScript into ES5 & auto-inject into browsers
 gulp.task('js', function(){
   return browserify({
-    entries: ['./src/main.js']
+    debug: true,
+    entries: ['./src/main.js'],
   })
     .transform(babelify.configure({
-      presets : ['es2015']
+      presets: ['es2015'],
+      sourceMapsAbsolute: true,
     }))
     .bundle()
+    .on("error", notify.onError(function (error) {
+      return "Error: " + error.message;
+    }))
     .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./public/js'))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.stream())
 });
