bb4e0e8b243a55fd12665ce70440055b96384c02
Chris Dorward <listingslab@gmail.com>
2017-06-06 14:55:56 +0930

    Add a Koa API https://github.com/ria-com/node-koajs-rest-skeleton

---
+++ api/app_console/controllers/defaultController.js
@@ -0,0 +1,10 @@
+/**
+ * @module controllers/defaultController
+ */
+async function index (options) {
+        console.log('Hello world defaultController & index action with options: ' +JSON.stringify(options));
+};
+
+module.exports = {index};
+
+
