18bc183a38cd7d4ffe11a64b512cc3fa9d98f0c9
Chris Dorward <listingslab@gmail.com>
2017-06-07 14:29:13 +0930

    vs 1.1.6

--- src/routes/router.js
+++
@@ -1,8 +0,0 @@
-
-import Router from 'koa-router'
-
-let router = new Router()
-
-router.get( '/status', require( './util/status' ) )
-
-export default router
--- src/util/log.js
+++
@@ -1,6 +0,0 @@
-
-import Logger from 'koa-bunyan-log'
-
-export default new Logger({
-    name: 'koa-api'
-})
